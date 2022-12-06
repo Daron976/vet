6e8e3f3f3423f6177cb0af8d32467d8cac7e3d75
Daron Wiafe <daronkwiafe@gmail.com>
2022-12-06 21:07:24 +0200

    Add new elements and transactions

--- queries.sql
+++ queries.sql
@@ -29,3 +29,48 @@ WHERE name <> 'Gabumon';
 SELECT *
 FROM animals
 WHERE weight_kg > 10.4 AND weight_kg < 17.3;
+
+BEGIN;
+
+UPDATE animals
+  SET species = 'unspecified';
+
+ROLLBACK;
+
+BEGIN;
+
+UPDATE animals
+  SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+  SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+BEGIN;
+
+DELETE FROM animals
+WHERE date_of_birth > 'Jan 1, 2022';
+
+SAVEPOINT sp1;
+
+UPDATE animals
+  SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO sp1;
+
+BEGIN;
+
+UPDATE animals
+  SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+COMMIT;
--- schema.sql
+++ schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE animals (
   date_of_birth date,
   escape_attempts int,
   neutered boolean,
-  weight_kg float
+  weight_kg float,
+  species varchar
 );
